121777f18032f8133e75fd340b7c4a3d74c81773
Poise-Paul <stargrades0@gmail.com>
2022-09-06 15:15:39 +0100

    mailtrap-setup

--- routes/ForgetPassword.js
+++ routes/ForgetPassword.js
@@ -2,11 +2,28 @@ import { Router } from "express";
 import { config } from "dotenv";
 import { User } from "../model/userSchemas.js";
 import jwt from "jsonwebtoken";
-
+import nodemailer from "nodemailer";
 // All Google credentials
 const router = new Router();
 let secret;
 // Send Mail
+const sendMailToken = async (resetToken) => {
+  const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "3534f9cd344b24",
+      pass: "18416972baa709",
+    },
+  });
+
+  await transport.sendMail({
+    from: "paulnseobong37@gmail.com",
+    to: "stargrades0@gmail.com",
+    subject: "Reset Password Token",
+    text: resetToken,
+  });
+};
 router
   .route("/")
   .post(async (req, res) => {
@@ -23,6 +40,7 @@ router
         };
         const token = jwt.sign(payload, secret, { expiresIn: "15m" });
         // console.log(token, findUser);
+        const result = await sendMailToken(token);
         res.status(200).send({ _id: findUser._id, token });
       } else {
         res.send("Invalid email");
@@ -37,7 +55,7 @@ router
     try {
       const isValid = jwt.verify(token, secret);
       const findUser = await User.findOne({ _id });
-      console.log(isValid, findUser);
+      console.log(findUser);
       const updateUser = await User.updateOne(
         { _id },
         { password },
