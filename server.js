640b2a16c51c56147c6be54c19c9633c6902f8ed
Poise-Paul <stargrades0@gmail.com>
2022-11-06 08:05:44 +0100

    bug-fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import `cors` from "cors";
+import cors from "cors";
 import dotenv from "dotenv";
 import { sendAlert } from "./routes/SendAlert.js";
 import { signIn } from "./routes/SignIn.js";
